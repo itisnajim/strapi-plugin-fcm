9f512ec6f8661474bef7ff31bc50376919f6e26f
Ognen Poposki <ognen.poposki@outlook.com>
2024-05-01 22:09:55 +0200

    Updated imports

--- server/services/fcm-notification.js
+++ server/services/fcm-notification.js
@@ -15,7 +15,7 @@ const {
     transformPaginationResponse,
 } = require('@strapi/strapi');
 
-const { getFetchParams } = require('@strapi/strapi');
+const { getFetchParams } = require('@strapi/strapi/lib/core-api/service');
 
 const {
     hasDraftAndPublish,
